d8abce9f5760a441b081126ca3f56a4e792ce1f3
Jean-yves BRISSET <jybrisset@gmail.com>
2019-09-12 13:46:23 +0200

    ft_printf complet

---
+++ ft_printf/libft/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrisset <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/30 11:08:46 by jbrisset          #+#    #+#             */
+/*   Updated: 2019/01/02 15:15:20 by jbrisset         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 21
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
